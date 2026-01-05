225cb57f87be48cd0fc0a1ef9e4cd581b8f5e8f8
dengliang shi <dengliangshi@yeah.net>
2026-01-05 21:35:14 +0800

    update

--- examples/run_predict.sh
+++ examples/run_predict.sh
@@ -3,4 +3,5 @@ python3 -m layoutgcn.predictor \
     --data-dir ./data/ \
     --model-path ./outputs/final_model/ \
     --output-dir ./outputs/ \
-    --batch-size 64
+    --batch-size 64 \
+    --do-evaluate True
